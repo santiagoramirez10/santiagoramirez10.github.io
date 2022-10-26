074c875449153bb6f8a03b74b9117d8e100afdcc
Santiago Ramirez <77732857+santiagoramirez10@users.noreply.github.com>
2022-10-26 11:42:51 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*menu*/
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () => {
@@ -8,3 +9,14 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-label", "Open menu");
   }
 });
+/*contact*/
+const $form = document.querySelector(".contact-form")
+const $buttonMailto = document.querySelector(".link-mail")
+$form.addEventListener('submit', handleSubmit)
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute('href', `mailto:santiagoramirezperez96@gmail.com?subject=nombre ${form.get('name')}, correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.click()
+  $form.reset();
+}

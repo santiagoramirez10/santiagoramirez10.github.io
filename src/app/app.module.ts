2bca1741753619ac8bbb4ee6ffb6944a41b105d3
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2023-03-15 18:41:30 -0500

    error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { routing } from './app.routing';
 
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { BlogComponent } from './blog/blog.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [

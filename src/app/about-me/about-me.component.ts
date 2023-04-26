8b37707f33bb583c8878a1ff0bda2effdcfba333
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2023-04-26 17:26:40 -0500

    Archivos json

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,5 +1,26 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import  CoursesJson  from "./courses.json"
+import  OtherCoursesJson  from "./other_courses.json"
+import LanguagesJson from "./languages.json"
+
+interface COURSES {
+  course: String;
+  school: String;
+  year: String;
+}
+
+interface OTHERCOURSES {
+  course: String;
+  school: String;
+  year: String;
+  link: String;
+}
+
+interface LANGUAGES{
+  language: String;
+  percentage: String;
+}
 
 @Component({
   selector: 'app-about-me',
@@ -7,8 +28,17 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent {
+  Courses: COURSES[] = CoursesJson;
+  OtherCourses: OTHERCOURSES[] = OtherCoursesJson;
+  Languages: LANGUAGES[] = LanguagesJson;
   constructor(private title: Title) {
     title.setTitle("Sobre mí-Portafolio Santiago Ramírez Pérez")
+    console.log(this.Courses);
+    console.log(this.OtherCourses);
+    console.log(this.Languages);
   }
+
+
+
 
 }

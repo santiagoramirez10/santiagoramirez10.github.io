da7cfb8954844269a0e9d05427fa576a1160bbad
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2023-07-04 18:34:44 -0500

    About me

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import  CoursesJson  from "./courses.json"
-import  OtherCoursesJson  from "./other_courses.json"
 import LanguagesJson from "./languages.json"
 
 interface COURSES {
   course: String;
   school: String;
   year: String;
-}
-
-interface OTHERCOURSES {
-  course: String;
-  school: String;
-  year: String;
-  link: String;
 }
 
 interface LANGUAGES{
@@ -29,12 +21,10 @@ interface LANGUAGES{
 })
 export class AboutMeComponent {
   Courses: COURSES[] = CoursesJson;
-  OtherCourses: OTHERCOURSES[] = OtherCoursesJson;
   Languages: LANGUAGES[] = LanguagesJson;
   constructor(private title: Title) {
     title.setTitle("Sobre mí-Portafolio Santiago Ramírez Pérez");
     console.log(this.Courses);
-    console.log(this.OtherCourses);
     console.log(this.Languages);
   }
 

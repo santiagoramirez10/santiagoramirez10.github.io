c711061f2ba35a6536ef3cb534798c1ad4b33182
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2023-08-14 15:53:35 -0500

    Work experience YouTube

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import  CoursesJson  from "./courses.json"
 import LanguagesJson from "./languages.json"
+import AchievementsJson from "./achievements.json"
 
 interface COURSES {
  img: String;
@@ -16,6 +17,10 @@ interface LANGUAGES{
   alt: String;
 }
 
+interface ACHIEVEMENTS{
+  title:String;
+  year: String;
+}
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
@@ -24,10 +29,12 @@ interface LANGUAGES{
 export class AboutMeComponent {
   Courses: COURSES[] = CoursesJson;
   Languages: LANGUAGES[] = LanguagesJson;
+  Achievements: ACHIEVEMENTS[]=AchievementsJson;
   constructor(private title: Title) {
     title.setTitle("Sobre mí-Portafolio Santiago Ramírez Pérez");
     console.log(this.Courses);
     console.log(this.Languages);
+    console.log(this.Achievements);
   }
 
 
